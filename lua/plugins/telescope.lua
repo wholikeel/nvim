f492f2c66c42f8e1a64c2b0c36d926d3805bcf2b
Michael LePera <mail@michaellepera.com>
2024-07-11 20:41:56 +0930

    Move util func to util.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,18 +4,6 @@ require("config.types")
 ---@field setup fun(opts?:table)
 ---@field load_extension fun(extension:string)
 
-function dump(o)
-   if type(o) == 'table' then
-      local s = '{ '
-      for k,v in pairs(o) do
-         if type(k) ~= 'number' then k = '"'..k..'"' end
-         s = s .. '['..k..'] = ' .. dump(v) .. ','
-      end
-      return s .. '} '
-   else
-      return tostring(o)
-   end
-end
 
 
 ---@type LazySpec
---
+++ lua/util.lua
@@ -0,0 +1,20 @@
+
+
+
+---String representation of a table
+---if param is not a table just calles
+---`tostring`.
+---@param o any
+---@return string
+_G.dump = function(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
