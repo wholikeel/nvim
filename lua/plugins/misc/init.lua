71c759bc46484f427487757c27db728cc18068f3
Michael LePera <mail@michaellepera.xyz>
2024-08-13 23:44:38 +0930

    add http-file viewer

---
+++ lua/plugins/misc/init.lua
@@ -0,0 +1,11 @@
+require("config.types")
+
+---@type LazySpec[]
+local M = {
+    require "plugins.misc.http",
+    -- require "plugins.editor.completion",
+    -- require "plugins.editor.lsp",
+}
+
+
+return M
