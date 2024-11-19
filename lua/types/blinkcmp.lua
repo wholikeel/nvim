3f555f1c8b25278a6e072484d4fb958eec38fa19
Michael LePera <mail@michaellepera.xyz>
2024-11-19 22:59:03 +1030

    feat: add blinkcmp types

---
+++ lua/types/blinkcmp.lua
@@ -0,0 +1,181 @@
+--- @alias blink.cmp.KeymapCommand
+--- | 'fallback' Fallback to the built-in behavior
+--- | 'show' Show the completion window
+--- | 'hide' Hide the completion window
+--- | 'accept' Accept the current completion item
+--- | 'select_and_accept' Select the current completion item and accept it
+--- | 'select_prev' Select the previous completion item
+--- | 'select_next' Select the next completion item
+--- | 'show_documentation' Show the documentation window
+--- | 'hide_documentation' Hide the documentation window
+--- | 'scroll_documentation_up' Scroll the documentation window up
+--- | 'scroll_documentation_down' Scroll the documentation window down
+--- | 'snippet_forward' Move the cursor forward to the next snippet placeholder
+--- | 'snippet_backward' Move the cursor backward to the previous snippet placeholder
+--- | (fun(cmp: table): boolean?) Custom function where returning true will prevent the next command from running
+
+--- @alias blink.cmp.KeymapPreset
+--- | 'default' mappings similar to built-in completion
+--- | 'super-tab' mappings similar to vscode (tab to accept, arrow keys to navigate)
+--- | 'enter' mappings similar to 'super-tab' but with 'enter' to accept
+
+--- @class blink.cmp.KeymapConfig
+--- @field preset? blink.cmp.KeymapPreset
+--- @field [string]? blink.cmp.KeymapCommand[]> Table of keys => commands[]
+
+--- @class blink.cmp.AcceptConfig
+--- @field expand_snippet? fun(string)
+--- @field create_undo_point? boolean Create an undo point when accepting a completion item
+--- @field auto_brackets? blink.cmp.AutoBracketsConfig
+
+--- @class blink.cmp.AutoBracketsConfig
+--- @field enabled? boolean
+--- @field default_brackets? string[]
+--- @field override_brackets_for_filetypes? table<string, string[] | fun(item: blink.cmp.CompletionItem): string[]>
+--- @field force_allow_filetypes? string[] Overrides the default blocked filetypes
+--- @field blocked_filetypes? string[]
+--- @field kind_resolution? blink.cmp.AutoBracketResolutionConfig Synchronously use the kind of the item to determine if brackets should be added
+--- @field semantic_token_resolution? blink.cmp.AutoBracketSemanticTokenResolutionConfig Asynchronously use semantic token to determine if brackets should be added
+
+--- @class blink.cmp.AutoBracketResolutionConfig
+--- @field enabled? boolean
+--- @field blocked_filetypes? string[]
+---
+--- @class blink.cmp.AutoBracketSemanticTokenResolutionConfig : blink.cmp.AutoBracketResolutionConfig
+--- @field timeout_ms? number How long to wait for semantic tokens to return before assuming no brackets should be added
+
+--- @class blink.cmp.CompletionTriggerConfig
+--- @field keyword_range? 'prefix' | 'full'
+--- @field keyword_regex? string
+--- @field exclude_from_prefix_regex? string
+--- @field blocked_trigger_characters? string[]
+--- @field show_on_accept_on_trigger_character? boolean When true, will show the completion window when the cursor comes after a trigger character after accepting an item
+--- @field show_on_insert_on_trigger_character? boolean When true, will show the completion window when the cursor comes after a trigger character when entering insert mode
+--- @field show_on_x_blocked_trigger_characters? string[] List of additional trigger characters that won't trigger the completion window when the cursor comes after a trigger character when entering insert mode/accepting an item
+--- @field show_in_snippet? boolean When false, will not show the completion window when in a snippet
+---
+--- @class blink.cmp.SignatureHelpTriggerConfig
+--- @field enabled? boolean
+--- @field blocked_trigger_characters? string[]
+--- @field blocked_retrigger_characters? string[]
+--- @field show_on_insert_on_trigger_character? boolean When true, will show the signature help window when the cursor comes after a trigger character when entering insert mode
+---
+--- @class blink.cmp.TriggerConfig
+--- @field completion? blink.cmp.CompletionTriggerConfig
+--- @field signature_help? blink.cmp.SignatureHelpTriggerConfig
+
+--- @class blink.cmp.SourceConfig
+--- @field completion? blink.cmp.SourceModeConfig
+--- @field providers? table<string, blink.cmp.SourceProviderConfig>
+---
+--- @class blink.cmp.SourceModeConfig
+--- @field enabled_providers? string[] | fun(ctx?: blink.cmp.Context): string[]
+---
+--- @class blink.cmp.SourceProviderConfig
+--- @field name string
+--- @field module? string
+--- @field enabled? boolean | fun(ctx?: blink.cmp.Context): boolean
+--- @field opts? table
+--- @field transform_items? fun(ctx: blink.cmp.Context, items: blink.cmp.CompletionItem[]): blink.cmp.CompletionItem[]
+--- @field should_show_items? boolean | number | fun(ctx: blink.cmp.Context, items: blink.cmp.CompletionItem[]): boolean
+--- @field max_items? number | fun(ctx: blink.cmp.Context, enabled_sources: string[], items: blink.cmp.CompletionItem[]): number
+--- @field min_keyword_length? number | fun(ctx: blink.cmp.Context, enabled_sources: string[]): number
+--- @field fallback_for? string[] | fun(ctx: blink.cmp.Context, enabled_sources: string[]): string[]
+--- @field score_offset? number | fun(ctx: blink.cmp.Context, enabled_sources: string[]): number
+--- @field deduplicate? blink.cmp.DeduplicateConfig
+--- @field override? blink.cmp.SourceOverride
+---
+--- @class blink.cmp.DeduplicateConfig
+--- @field enabled? boolean
+--- @field priority? number
+
+--- @class blink.cmp.PrebuiltBinariesConfig
+--- @field download? boolean
+--- @field force_version? string | nil
+--- @field force_system_triple? string | nil
+
+--- @class blink.cmp.FuzzyConfig
+--- @field use_typo_resistance? boolean
+--- @field use_frecency? boolean
+--- @field use_proximity? boolean
+--- @field max_items? number
+--- @field sorts? ("label" | "kind" | "score")[]
+--- @field prebuilt_binaries? blink.cmp.PrebuiltBinariesConfig
+
+--- @class blink.cmp.WindowConfig
+--- @field autocomplete? blink.cmp.AutocompleteConfig
+--- @field documentation? blink.cmp.DocumentationConfig
+--- @field signature_help? blink.cmp.SignatureHelpConfig
+--- @field ghost_text? GhostTextConfig
+
+--- @class blink.cmp.HighlightConfig
+--- @field ns? number
+--- @field use_nvim_cmp_as_default? boolean
+
+--- @class blink.cmp.AutocompleteConfig
+--- @field min_width? number
+--- @field max_height? number
+--- @field border? blink.cmp.WindowBorder
+--- @field scrollbar? boolean
+--- @field order? "top_down" | "bottom_up"
+--- @field direction_priority? ("n" | "s")[]
+--- @field auto_show? boolean
+--- @field selection? "preselect" | "manual" | "auto_insert"
+--- @field winblend? number
+--- @field winhighlight? string
+--- @field scrolloff? number
+--- @field draw? blink.cmp.Draw
+--- @field cycle? blink.cmp.AutocompleteConfig.CycleConfig
+
+--- @class blink.cmp.AutocompleteConfig.CycleConfig
+--- @field from_bottom? boolean When `true`, calling `select_next` at the *bottom* of the completion list will select the *first* completion item.
+--- @field from_top? boolean When `true`, calling `select_prev` at the *top* of the completion list will select the *last* completion item.
+
+--- @class blink.cmp.DocumentationDirectionPriorityConfig
+--- @field autocomplete_north? ("n" | "s" | "e" | "w")[]
+--- @field autocomplete_south? ("n" | "s" | "e" | "w")[]
+---
+--- @alias blink.cmp.WindowBorderChar string | table
+--- @alias blink.cmp.WindowBorder 'single' | 'double' | 'rounded' | 'solid' | 'shadow' | 'padded' | 'none' | blink.cmp.WindowBorderChar[]
+---
+--- @class blink.cmp.DocumentationConfig
+--- @field min_width? number
+--- @field max_width? number
+--- @field max_height? number
+--- @field desired_min_width? number
+--- @field desired_min_height? number
+--- @field border? blink.cmp.WindowBorder
+--- @field winblend? number
+--- @field winhighlight? string
+--- @field scrollbar? boolean
+--- @field direction_priority? blink.cmp.DocumentationDirectionPriorityConfig
+--- @field auto_show? boolean
+--- @field auto_show_delay_ms? number Delay before showing the documentation window
+--- @field update_delay_ms? number Delay before updating the documentation window
+--- @field treesitter_highlighting? boolean Whether to use treesitter highlighting, disable if you run into performance issues
+
+--- @class blink.cmp.SignatureHelpConfig
+--- @field min_width? number
+--- @field max_width? number
+--- @field max_height? number
+--- @field border? blink.cmp.WindowBorder
+--- @field winblend? number
+--- @field winhighlight? string
+--- @field scrollbar? boolean
+--- @field direction_priority? ("n" | "s")[]
+--- @field treesitter_highlighting? boolean Whether to use treesitter highlighting, disable if you run into performance issues
+
+--- @class GhostTextConfig
+--- @field enabled? boolean
+
+--- @class blink.cmp.Config
+--- @field keymap? blink.cmp.KeymapConfig | blink.cmp.KeymapPreset
+--- @field accept? blink.cmp.AcceptConfig
+--- @field trigger? blink.cmp.TriggerConfig
+--- @field fuzzy? blink.cmp.FuzzyConfig
+--- @field sources? blink.cmp.SourceConfig
+--- @field windows? blink.cmp.WindowConfig
+--- @field highlight? blink.cmp.HighlightConfig
+--- @field nerd_font_variant? 'mono' | 'normal'
+--- @field kind_icons? table<string, string>
+--- @field blocked_filetypes? string[]
