277246c4f649ad45f6fccdfc22c194b651276df4
Michael LePera <mail@michaellepera.xyz>
2025-07-08 20:25:37 +0930

    feat: initial

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Neovim RC configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/24.11";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, ... }@inputs:
+    inputs.utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        formatter = pkgs.nixfmt-rfc-style;
+        devShells.default = pkgs.callPackage ./shell.nix { };
+      }
+    );
+}
